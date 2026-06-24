6a859a0136d10861eed236e08f5cdaa25b817070
风中有朵雨做的云 <2848009772@qq.com>
2026-06-24 16:44:18 +0800

    fix: 新增仅预览功能

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import './assets/main.css';
 import 'md-editor-v3/lib/style.css';
+import 'md-editor-v3/lib/preview.css';
 
 const app = createApp(App);
 app.use(createPinia());
